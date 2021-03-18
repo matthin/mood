19aa2f9cd542bb3a3f7d5d92ac2b3a4627d73950
Justin <me@justinharrison.ca>
2021-03-18 14:50:19 -0400

    Forgot another hardcoded url

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -5,7 +5,7 @@ export default function CallbackPage({ location }) {
     const params = new URLSearchParams(location.hash.substring(1));
     sessionStorage.setItem("token", params.get("access_token"));
 
-    window.location.href = "http://localhost:8000/mood";
+    window.location.href = `${window.location.protocol}//${window.location.host}/mood`;
   }, [location.hash]);
 
   return <></>;
